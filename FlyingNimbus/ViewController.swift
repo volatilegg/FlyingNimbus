2ea5dd6b98ccf8f1736e90a9ca4eb5668b4b8068
DucDo <duc.fet01@gmail.com>
2016-05-21 17:16:46 +0300

    Initial Commit

---
+++ FlyingNimbus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlyingNimbus
+//
+//  Created by Do Duc on 21/05/16.
+//  Copyright © 2016 Do Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
